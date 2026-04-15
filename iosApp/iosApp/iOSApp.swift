9c77038348c38dc43860875431a727d5afae5346
Taki <bmtaki335@gmail.com>
2026-04-15 16:31:07 +0900

    Add Firebase iOS SDK

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 import Di
+import FirebaseCore
 
 @main
 struct iOSApp: App {
     init() {
         startKoin()
+        FirebaseApp.configure()
     }
     
     var body: some Scene {
