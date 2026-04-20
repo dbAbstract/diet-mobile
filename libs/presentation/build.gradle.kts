5bd207b6eb6f84bb84da28d2bc4cfb8af4f02b02
Taki <bmtaki335@gmail.com>
2026-04-20 18:32:01 +0900

    Add libs:presentation module and KmpObservable Swift wrapper

---
+++ libs/presentation/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("yaseyo.kmp.library")
+}
+
+kotlin {
+    androidLibrary {
+        namespace = "dev.yaseyo.presentation"
+
+        withHostTestBuilder {
+        }
+
+        withDeviceTestBuilder {
+            sourceSetTreeName = "test"
+        }.configure {
+            instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(libs.androidx.lifecycle.viewmodel)
+                api(libs.kotlinx.coroutines.core)
+            }
+        }
+
+        commonTest {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+
+        androidMain {
+            dependencies {
+            }
+        }
+
+        getByName("androidDeviceTest") {
+            dependencies {
+                implementation(libs.androidx.runner)
+                implementation(libs.androidx.core)
+                implementation(libs.androidx.testExt.junit)
+            }
+        }
+
+        iosMain {
+            dependencies {
+            }
+        }
+    }
+}
