5a0b0fd367db639ba0b9fd67c3983fa8e3d7582a
Taki <bmtaki335@gmail.com>
2026-05-15 20:22:49 +0900

    Rename ModalRoute to RouteMarkers and add AppRootRoute interface

--- feature/onboarding/src/commonMain/kotlin/dev/yaseyo/onboarding/navigation/OnboardingRoutes.kt
+++ feature/onboarding/src/commonMain/kotlin/dev/yaseyo/onboarding/navigation/OnboardingRoutes.kt
@@ -1,11 +1,12 @@
 package dev.yaseyo.onboarding.navigation
 
+import dev.yaseyo.navigation.AppRootRoute
 import dev.yaseyo.navigation.AppRoute
 import dev.yaseyo.navigation.ModalRoute
 
 sealed interface OnboardingRoutes : AppRoute {
     data object Auth : OnboardingRoutes {
-        data object Landing : OnboardingRoutes
+        data object Landing : OnboardingRoutes, AppRootRoute
 
         data object SignIn : OnboardingRoutes, ModalRoute
 
--- libs/navigation/src/commonMain/kotlin/dev/yaseyo/navigation/RouteMarkers.kt
+++ libs/navigation/src/commonMain/kotlin/dev/yaseyo/navigation/RouteMarkers.kt
@@ -1,3 +1,5 @@
 package dev.yaseyo.navigation
 
 interface ModalRoute
+
+interface AppRootRoute
